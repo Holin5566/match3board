3a79f551c2a334020e0478aa9b08f1f62eea1af5
HOLIN-CHEN-NB\holin_chen <holin_chen@paradise-soft.com.tw>
2023-04-04 02:06:08 +0800

    完成三消

--- Script/Board.ts
+++ Script/Board.ts
@@ -1,7 +1,7 @@
 import GridSystem from "./GridSystem";
 import Orb from "./Orb";
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 export enum EventType {
     ORB_PICK = "ORB_PICK",
@@ -44,12 +44,12 @@ export default class Board extends cc.Component {
 
     // onLoad () {}
 
-    start () {
+    start() {
         this.initGrid();
 
         cc.systemEvent.on(EventType.ORB_EXCHANGE, this.onOrbExchange, this);
         cc.systemEvent.on(EventType.ORB_MATCH, this.onOrbMatch, this);
-        cc.systemEvent.on(EventType.ORB_DROP, this.onOrbDrop, this);
+        cc.systemEvent.on(EventType.ORB_PICK, this.onOrbPick, this);
     }
 
     // update (dt) {}
@@ -57,8 +57,7 @@ export default class Board extends cc.Component {
     protected onDestroy(): void {
         cc.systemEvent.off(EventType.ORB_EXCHANGE, this.onOrbExchange, this);
         cc.systemEvent.off(EventType.ORB_MATCH, this.onOrbMatch, this);
-        cc.systemEvent.off(EventType.ORB_DROP, this.onOrbDrop, this);
-
+        cc.systemEvent.off(EventType.ORB_PICK, this.onOrbPick, this);
     }
 
     /** 初始化網格 */
@@ -66,13 +65,13 @@ export default class Board extends cc.Component {
         let gridSize = cc.v2(this.gridCol, this.gridRow);
         let ordSize = cc.v2(this.orbPrefab.width, this.orbPrefab.height);
         let gap = this.gap;
-        this._grid.init(gridSize, ordSize, gap)
+        this._grid.init(gridSize, ordSize, gap);
 
-        for(let col = 0; col < gridSize.x; col++) {
-            for(let row = 0; row < gridSize.y; row++) {
+        for (let col = 0; col < gridSize.x; col++) {
+            for (let row = 0; row < gridSize.y; row++) {
                 const orbNode = cc.instantiate(this.orbPrefab);
                 const orbItem = orbNode.getComponent(Orb);
-                
+
                 orbNode.setParent(this.gridNode);
                 orbNode.setPosition(this._grid.getPos(cc.v2(col, row)));
 
@@ -92,6 +91,9 @@ export default class Board extends cc.Component {
         this.blockNode.active = false;
     }
 
+    private onOrbPick(orb: Orb) {
+        this._passedOrbs.push(orb);
+    }
     /** 監聽圖標互換 */
     private onOrbExchange(mainCoord: cc.Vec2, targetCoord: cc.Vec2) {
         cc.warn(`onOrbExchange: [${mainCoord.x}, ${mainCoord.y}] => [${targetCoord.x}, ${targetCoord.y}]`);
@@ -106,9 +108,9 @@ export default class Board extends cc.Component {
     }
 
     /** 監聽圖標消除 */
-    private async onOrbMatch(e: cc.Touch, orb: Orb) {
-        cc.warn(`onOrbPut: [${orb.coord.x}, ${orb.coord.y}]`);
+    private async onOrbMatch() {
         const match3List = [] as Orb[];
+        const dropList: Orb[][] = [];
         this._passedOrbs.forEach(orb => {
             cc.warn(`passedOrb: [${orb.coord.x}, ${orb.coord.y}]`);
             match3List.push(...this._grid.getMatch3List(orb.coord));
@@ -116,94 +118,86 @@ export default class Board extends cc.Component {
         this._passedOrbs.length = 0;
 
         // 消除動畫
-        match3List.sort((a, b) => a.coord.y - b.coord.y);
-        for(let i = 0; i < match3List.length; i++) {
-            match3List[i].isMatch = false;
-            if (i === match3List.length - 1) {
-                await match3List[i].playWinEffect();
-            } else {
-                match3List[i].playWinEffect();
-            }
-        };
-        // // 掉落動畫
-        // for(let i = 0; i < match3List.length; i++) {
-        //     const main = match3List[i];
+        match3List.sort((a, b) => a.coord.y - b.coord.y); // y軸由大到小，由下往上消除
+        await Promise.all(match3List.map(orb => orb.playWinEffect()));
 
-        //     matchCount[main.coord.x] += 1;
-        //     let originY = main.coord.y;
-        //     this._passedOrbs.push(main);
-
-        //     // 調整座標
-        //     for(let curY = originY; curY > 0; curY--) {
-        //         const lowerOrb = main;
-        //         const upperOrb = this._grid.getItem(cc.v2(main.coord.x, main.coord.y - 1));
-    
-        //         this._passedOrbs.push(upperOrb);
-        //         this._grid.exchangeItem(lowerOrb.coord, upperOrb.coord);
-        //         const lowerCoord = lowerOrb.coord;
-        //         const upperCoord = upperOrb.coord;
-        //         if (curY === 1) {
-        //             lowerOrb.transformTo(upperCoord, false);
-        //             upperOrb.transformTo(lowerCoord, false);
-        //         } else {
-        //             upperOrb.transformTo(lowerCoord, false);
-        //             lowerOrb.transformTo(upperCoord, false);
-        //         }
-        //     }
-        //     // 移動到最上方
-        //     // main.node.y = this.gridRow * (this.gap + this.orbPrefab.height);
-        //     // cc.tween(main.node)
-        //     //     .to(1, { position: cc.v3(this._grid.getPos(main.coord)) }, {easing: cc.easing.quadOut})
-        //     //     .call(() => {
-        //     //         if (i === match3List.length - 1) {
-        //     //             this.onOrbMatch(null, main);
-        //     //         }
-        //     //     })
-        //     //     .start();
-
-        //     // main.setType(null);
-
-        //     cc.error("main", main.coord.x,"-", main.coord.y,"after")
-            
-        //     // 必須等所有盤面 drop 完畢才能進行 match
-        //     this.scheduleOnce(() => {
-        //         this.onOrbMatch(null, main);
-        //         main.node.active = true;
-        //     });
-        // };
-
-    }
-
-    /** 監聽圖標掉落 */
-    private async onOrbDrop(main: Orb) {
-        let originY = main.coord.y;
-        this._passedOrbs.push(main);
-        main.node.active = false;
-
-        for(let curY = originY; curY > 0; curY--) {
-            const lowerOrb = main;
-            const upperOrb = this._grid.getItem(cc.v2(main.coord.x, main.coord.y - 1));
-
-            this._passedOrbs.push(upperOrb);
-            const lowerCoord = lowerOrb.coord;
-            const upperCoord = upperOrb.coord;
-            if (curY === 1) {
-                lowerOrb.transformTo(upperCoord, false);
-                upperOrb.transformTo(lowerCoord, false);
-            } else {
-                upperOrb.transformTo(lowerCoord, false);
-                lowerOrb.transformTo(upperCoord, false);
-            }
-            this._grid.exchangeItem(lowerOrb.coord, upperOrb.coord);
-        }
-        main.setType(null);
-        main.node.active = true;
-        cc.error("main", main.coord.x,"-", main.coord.y,"after")
-        
-        // 必須等所有盤面 drop 完畢才能進行 match
-        this.scheduleOnce(() => {
-            this.onOrbMatch(null, main);
-            main.node.active = true;
+        // 消除的圖標移動到畫面外上方
+        const winCount: number[] = []; // 該行消除的圖標計數
+        match3List.forEach(winOrb => {
+            const { x } = winOrb.coord;
+            winCount[x] = winCount[x] ? winCount[x] + 1 : 1;
+            winOrb.node.y = winOrb.node.height * winCount[x];
         });
+
+        // 處理網格座標
+        match3List.forEach(originOrb => {
+            const { x: originX, y: originY } = originOrb.coord;
+            dropList[originX] = dropList[originX] ? dropList[originX] : [];
+            for (let curY = originY - 1; curY >= 0; curY--) {
+                const curOrb = this._grid.getItem(cc.v2(originX, curY));
+                if (!dropList[originX].includes(curOrb) && !curOrb.isMatch) {
+                    dropList[originX].push(curOrb);
+                }
+                curOrb.setCoord(cc.v2(originX, curY + 1));
+                this._grid.insertItem(cc.v2(originX, curY + 1), curOrb);
+            }
+            if (!dropList[originX].includes(originOrb)) {
+                dropList[originX].push(originOrb);
+            }
+            originOrb.setCoord(cc.v2(originX, 0));
+            this._grid.insertItem(cc.v2(originX, 0), originOrb);
+        });
+
+
+        // 掉落動畫
+        const dropingTween: Promise<void>[] = [];
+        dropList.forEach((dropOrbs) => {
+            dropOrbs.forEach((dropOrb, dropCount) => {
+                const raito = dropCount / dropOrbs.length;
+                this._passedOrbs.push(dropOrb);
+                dropingTween.push(dropOrb.drop(raito));
+            });
+        });
+
+        match3List.forEach(orb => {
+            orb.setType();
+            orb.isMatch = false;
+        });
+
+        await Promise.all(dropingTween);
+        if (match3List.length > 0) this.onOrbMatch();
     }
+
+    // /** 監聽圖標掉落 */
+    // private async onOrbDrop(main: Orb) {
+    //     let originY = main.coord.y;
+    //     this._passedOrbs.push(main);
+    //     main.node.active = false;
+
+    //     for (let curY = originY; curY > 0; curY--) {
+    //         const lowerOrb = main;
+    //         const upperOrb = this._grid.getItem(cc.v2(main.coord.x, main.coord.y - 1));
+
+    //         this._passedOrbs.push(upperOrb);
+    //         const lowerCoord = lowerOrb.coord;
+    //         const upperCoord = upperOrb.coord;
+    //         if (curY === 1) {
+    //             lowerOrb.transformTo(upperCoord, false);
+    //             upperOrb.transformTo(lowerCoord, false);
+    //         } else {
+    //             upperOrb.transformTo(lowerCoord, false);
+    //             lowerOrb.transformTo(upperCoord, false);
+    //         }
+    //         this._grid.exchangeItem(lowerOrb.coord, upperOrb.coord);
+    //     }
+    //     main.setType();
+    //     main.node.active = true;
+    //     cc.error("main", main.coord.x, "-", main.coord.y, "after");
+
+    //     // 必須等所有盤面 drop 完畢才能進行 match
+    //     this.scheduleOnce(() => {
+    //         this.onOrbMatch();
+    //         main.node.active = true;
+    //     });
+    // }
 }
--- Script/Orb.ts
+++ Script/Orb.ts
@@ -9,13 +9,14 @@ export default class Orb extends cc.Component {
     @property(sp.Skeleton)
     spine: sp.Skeleton = null;
 
-    // @property(cc.Label)
-    // label: cc.Label = null;
+    @property(cc.Label)
+    label: cc.Label = null;
 
     public isMatch: boolean = false;
 
     private _coord: cc.Vec2 = null;
     public get coord(): cc.Vec2 { return this._coord; }
+    public get coordStr(): string { return `${this._coord.x}-${this._coord.y}`; }
 
     private _type: OrbType = null;
     public get type(): OrbType { return this._type; }
@@ -24,6 +25,7 @@ export default class Orb extends cc.Component {
     private _coordBeforMove: cc.Vec2 = null;
     private _posBeforMove: cc.Vec2 = null;
     private _isMoving: boolean = false;
+    private _isPlaying: boolean = false;
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
@@ -44,7 +46,6 @@ export default class Orb extends cc.Component {
     public init(coord: cc.Vec2, type: OrbType) {
         this._coord = coord;
         this.setType(type);
-        // this.label.string = `${coord.x} - ${coord.y}`;
 
         // 綁定觸摸事件
         this.node.on(cc.Node.EventType.TOUCH_START, this.onTouchStart, this);
@@ -67,45 +68,52 @@ export default class Orb extends cc.Component {
                 this.node.setPosition(GridSystem.instance.getPos(coord));
             }
         })
-        // this.label.string = `${coord.x} - ${coord.y}`;
     }
 
-    public async dropTo(coord: cc.Vec2, isFinal: boolean = false) {
-        this._coord = coord;
-        // this.label.string = `${coord.x} - ${coord.y}`;
+    // 掉落動畫 (根據自身座標掉落，使用前先設定到對應座標)
+    public async drop(raito: number): Promise<void> {
+        const base = 0.3;
+        const duration = 0.5;
+        const before = duration * raito;
+        const during = duration - before + base;
         return new Promise((resolve, reject) => {
             cc.tween(this.node)
-                .to(1, { position: cc.v3(GridSystem.instance.getPos(coord)) }, { easing: cc.easing.quadIn })
+                .delay(before)
+                .to(during, { position: cc.v3(GridSystem.instance.getPos(this._coord)) }, { easing: cc.easing.quadOut })
                 .call(resolve)
                 .start();
         });
     }
     /** 設置屬性 */
-    public setType(type: OrbType) {
+    public setType(type?: OrbType) {
         if (!type) {
             this._type = [OrbType.H1, OrbType.H2, OrbType.N1, OrbType.N2, OrbType.N3, OrbType.N4][Math.floor(Math.random() * 6)];
         } else {
             this._type = type;
         }
         this.spine.setAnimation(0, `${this._type}_play_normal`, true);
+        this.label.string = `${this._coord.x} - ${this._coord.y}`;
     }
 
     /** 設置座標 */
     public setCoord(coord: cc.Vec2) {
         this._coord = coord;
-        // this.label.string = `${coord.x} - ${coord.y}`;
+        this.label.string = `${coord.x} - ${coord.y}`;
     }
 
     /** 播放勝利特效 */
     public async playWinEffect() {
+        if (this._isPlaying) {
+            return;
+        }
+        this._isPlaying = true;
         return new Promise((resolve, reject) => {
             this.spine.setAnimation(0, `${this._type}_play_win`, false);
             cc.error(`${this._coord.y} - ${this._coord.x}`);
             this.spine.setCompleteListener(() => {
                 this.spine.setCompleteListener(null);
-                this.spine.setAnimation(0, `${this._type}_play_normal`, true);
+                this._isPlaying = false;
                 resolve("");
-                cc.systemEvent.emit(EventType.ORB_DROP, this);
             });
         });
     }
@@ -116,7 +124,7 @@ export default class Orb extends cc.Component {
         this._posBeforMove = this.node.getPosition();
         this._coordBeforMove = this._coord;
 
-        cc.systemEvent.emit(EventType.ORB_PICK, e);
+        cc.systemEvent.emit(EventType.ORB_PICK, this);
     }
 
     /** 監聽點擊移動 */
